915892e6b97f3c450ca153b0358fa4d5de034527
Martin Disch <martindisch@gmail.com>
2023-10-31 20:13:16 +0100

    feat: Add empty encode command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Parser, Subcommand};
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Encode { id } => println!("Got {id}"),
+    }
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Encode an ID.
+    Encode {
+        /// The ID.
+        id: u32,
+    },
 }
