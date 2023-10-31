c8d97bb5ce0b04eb11e83afe4f8cd0b0a079cd1a
Martin Disch <martindisch@gmail.com>
2023-10-31 21:47:56 +0100

    feat: Implement serialize command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+use base64::{engine::general_purpose, Engine};
 use clap::{Parser, Subcommand};
+
+// Implementation of ID serialization according to
+// https://github.com/ChilliCream/graphql-platform/blob/main/src/HotChocolate/Core/src/Types/Types/Relay/IdSerializer.cs
 
 fn main() {
     let cli = Cli::parse();
     match cli.command {
-        Commands::Encode { id } => println!("Got {id}"),
+        Commands::Encode { name, id } => {
+            println!(
+                "{}",
+                general_purpose::STANDARD_NO_PAD.encode(format!("{name}\ni{id}"))
+            )
+        }
     }
 }
 
@@ -18,6 +27,8 @@ struct Cli {
 enum Commands {
     /// Encode an ID.
     Encode {
+        /// The name of the type.
+        name: String,
         /// The ID.
         id: u32,
     },
