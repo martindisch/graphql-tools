425536976eb538bc57290c32b0e883cf03d62045
Martin Disch <martindisch@gmail.com>
2023-11-05 17:37:06 +0100

    feat: Add type option

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::fmt::Display;
+
 use base64::{engine::general_purpose, Engine};
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 
 // Implementation of ID serialization according to
 // https://github.com/ChilliCream/graphql-platform/blob/main/src/HotChocolate/Core/src/Types/Types/Relay/IdSerializer.cs
@@ -7,10 +9,10 @@ use clap::{Parser, Subcommand};
 fn main() {
     let cli = Cli::parse();
     match cli.command {
-        Commands::Encode { name, id } => {
+        Commands::Encode { name, id, id_type } => {
             println!(
                 "{}",
-                general_purpose::STANDARD_NO_PAD.encode(format!("{name}\ni{id}"))
+                general_purpose::STANDARD_NO_PAD.encode(format!("{name}\n{id_type}{id}"))
             )
         }
     }
@@ -31,5 +33,28 @@ enum Commands {
         name: String,
         /// The ID.
         id: u32,
+        /// The data type.
+        #[arg(short = 't', long = "type", default_value_t = Type::I, value_enum)]
+        id_type: Type,
     },
+}
+
+#[derive(Clone, ValueEnum)]
+enum Type {
+    /// int
+    I,
+    /// long
+    L,
+    /// string
+    S,
+}
+
+impl Display for Type {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Type::I => write!(f, "i"),
+            Type::L => write!(f, "l"),
+            Type::S => write!(f, "d"),
+        }
+    }
 }
