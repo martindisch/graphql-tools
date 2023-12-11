c1d8bef00ee2cfa58454446de1ff955fdb303ba4
Martin Disch <martindisch@gmail.com>
2023-12-11 16:39:01 +0100

    refactor: Move GID functionality into type

---
+++ src/gid.rs
@@ -0,0 +1,87 @@
+use std::{
+    fmt::{Debug, Display},
+    str,
+};
+
+use base64::{engine::general_purpose, Engine};
+use clap::ValueEnum;
+use eyre::{eyre, Context, Report, Result};
+
+pub struct Gid {
+    pub name: String,
+    pub id: String,
+    pub id_type: Type,
+}
+
+impl TryFrom<String> for Gid {
+    type Error = Report;
+
+    fn try_from(id: String) -> Result<Self, Self::Error> {
+        let id_bytes = general_purpose::STANDARD
+            .decode(id)
+            .wrap_err("Input is not valid base64")?;
+        let id_string = str::from_utf8(&id_bytes).wrap_err("Input is not valid UTF-8")?;
+
+        match id_string.split_once('\n') {
+            Some((name, type_and_id)) if type_and_id.len() >= 2 => {
+                let id_type = Type::try_from(&type_and_id[..1])?;
+                let id = &type_and_id[1..];
+                Ok(Self {
+                    name: name.into(),
+                    id: id.into(),
+                    id_type,
+                })
+            }
+            _ => Err(eyre!("Input is not a valid GID"))?,
+        }
+    }
+}
+
+impl Display for Gid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            general_purpose::STANDARD.encode(format!("{}\n{}{}", self.name, self.id_type, self.id))
+        )
+    }
+}
+
+impl Debug for Gid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} ({})", self.name, self.id, self.id_type)
+    }
+}
+
+#[derive(Clone, ValueEnum)]
+pub enum Type {
+    /// int
+    I,
+    /// long
+    L,
+    /// string
+    S,
+}
+
+impl TryFrom<&str> for Type {
+    type Error = Report;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "i" => Ok(Self::I),
+            "l" => Ok(Self::L),
+            "d" => Ok(Self::S),
+            _ => Err(eyre!("{value} is not a valid ID type")),
+        }
+    }
+}
+
+impl Display for Type {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Type::I => write!(f, "i"),
+            Type::L => write!(f, "l"),
+            Type::S => write!(f, "d"),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::{fmt::Display, str};
-
 use arboard::Clipboard;
-use base64::{engine::general_purpose, Engine};
-use clap::{Parser, Subcommand, ValueEnum};
-use eyre::{eyre, Context, Result};
+use clap::{Parser, Subcommand};
+use eyre::Result;
+
+mod gid;
+use gid::{Gid, Type};
 
 // Implementation of ID serialization according to
 // https://github.com/ChilliCream/graphql-platform/blob/main/src/HotChocolate/Core/src/Types/Types/Relay/IdSerializer.cs
@@ -13,25 +13,15 @@ fn main() -> Result<()> {
     let mut clipboard = Clipboard::new()?;
     match cli.command {
         Commands::Encode { name, id, id_type } => {
-            let id = general_purpose::STANDARD.encode(format!("{name}\n{id_type}{id}"));
-            clipboard.set_text(&id)?;
-            println!("{id}",)
+            let gid = Gid { name, id, id_type };
+            let encoded_gid = gid.to_string();
+            clipboard.set_text(&encoded_gid)?;
+            println!("{encoded_gid}")
         }
         Commands::Decode { id } => {
-            let id_bytes = general_purpose::STANDARD
-                .decode(id)
-                .wrap_err("Input is not valid base64")?;
-            let id_string = str::from_utf8(&id_bytes).wrap_err("Input is not valid UTF-8")?;
-
-            match id_string.split_once('\n') {
-                Some((name, type_and_id)) if type_and_id.len() >= 2 => {
-                    let id_type = &type_and_id[..1];
-                    let id = &type_and_id[1..];
-                    clipboard.set_text(id)?;
-                    println!("{name} {id} ({id_type})")
-                }
-                _ => Err(eyre!("Input is not a valid GID"))?,
-            }
+            let gid = Gid::try_from(id)?;
+            clipboard.set_text(&gid.id)?;
+            println!("{gid:#?}");
         }
     }
 
@@ -62,24 +52,4 @@ enum Commands {
         /// ID you want to decode, e.g. UHJvZHVjdAppMTIzNA==.
         id: String,
     },
-}
-
-#[derive(Clone, ValueEnum)]
-enum Type {
-    /// int
-    I,
-    /// long
-    L,
-    /// string
-    S,
-}
-
-impl Display for Type {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Type::I => write!(f, "i"),
-            Type::L => write!(f, "l"),
-            Type::S => write!(f, "d"),
-        }
-    }
 }
